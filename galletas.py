8a5add1fb3238384d59653cd4c1754db7ff070c3
Sebastuchoo <sebastianantoniolopezmojica3@gmail.com>
2026-08-10 09:48:32 -0500

    Rename method to sabor_galleta for clarity

--- galletas.py
+++ galletas.py
@@ -2,8 +2,8 @@ class Galleta:
     def __init__(self, sabor):
         self.sabor = sabor
 
-    def sabor(self):
-            return f"Galleta de {self.sabor}"
+    def sabor_galleta(self):
+        return f"Galleta de {self.sabor}"
 
-g1= Galleta ("Chocolate")
-print(g1.sabor)
+g1= Galleta("Chocolate")
+print(g1.sabor_galleta())
